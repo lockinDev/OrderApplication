47a390e2367eb8ff79fe3404d6441394fd9e7ca0
Javier <javieralmario12@gmail.com>
2022-01-26 22:18:02 -0400

    Added principal classes for the order application. Added Order, LineItem, Invoice, with the service and exception class.

---
+++ src/main/java/com/devlockin/order/OrderApplication.java
@@ -0,0 +1,14 @@
+package com.devlockin.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String args[]) {
+
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
