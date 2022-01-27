6e27e26c5701985d8242ebe196e2e25251eeffd8
Javier <javieralmario12@gmail.com>
2022-01-27 08:31:02 -0400

    Added auth server with basic authorization client_credentials, between two microservices. Added auth configuration on order service.

--- src/main/java/com/devlockin/order/service/OrderProcesingService.java
+++ src/main/java/com/devlockin/order/service/OrderProcesingService.java
@@ -4,8 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,4 +48,7 @@ public class OrderProcesingService {
             throw new OrderNotFoundException();
         }
     }
+    
+
+    
 }
